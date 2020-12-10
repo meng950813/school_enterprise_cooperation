69eb9778a809828a436a710841e0f76841d1aa39
i2lmeng <1752397568@qq.com>
2020-12-10 20:30:44 +0800

    modify: 删除功能性函数，使index文件专门提供用户验证、注销等公共功能

--- web/blueprints/social_network/index.py
+++ web/blueprints/social_network/index.py
@@ -1,48 +1,56 @@
-from flask import Blueprint, render_template, request
-from web.service.social_network import recommend as recommend_service
+from flask import Blueprint, redirect, url_for, abort, request, render_template
 from web.service.social_network import public as public_service
-from web.service.social_network import recommend_detail as detail_service
+from web.service.social_network import personal_network as personal_service
+from web.extensions import oidc
+from web.utils import auth
+import requests
 
-recommend_bp = Blueprint('recommend', __name__)
+index_bp = Blueprint('index', __name__)
 
 
-@recommend_bp.route("/")
-@recommend_bp.route("/index")
+@index_bp.route("/")
+@index_bp.route("/index")
+@oidc.require_login
 def index():
-    universities = public_service.getUniversityList(limit=150)
-    return render_template("social_network/index.html", universities=universities)
+    if auth.require_role("KETD", "技转中心"):
+        # 高校技转中心用户
+        return redirect(url_for("recommend2Area.index"))
+    elif auth.require_role("KETD", "孵化器"):
+        # 地区中介用户
+        pass
+    else:
+        abort(404)
 
 
-@recommend_bp.route("/recommendTeacherForArea", methods=["GET"])
-def recommendTeacherForArea():
-    u_id = request.args.get("uni", default="", type=str)
-    skip = request.args.get("pageNum", default=0, type=int)
-    limit = request.args.get("pageSize", default=10, type=int)
-    sort = request.args.get("sort", type=str, default="")
-    order = request.args.get("order", type=str, default="desc")
-    return recommend_service.recommendTeacherForArea(area_id=2, uni_id=u_id, sort=sort, order=order,
-                                                     skip=skip, limit=limit)
+@index_bp.route("/logout")
+def logout():
+    # back_host = "39.98.94.44"
+    back_host = "127.0.0.1:5000"
+    url = "http://39.98.94.44:8085/auth/realms/kunshan/protocol/openid-connect/logout?" \
+          "redirect_uri=http://{back_host}/logout".format(back_host=back_host)
+    response = requests.get(url=url)
+
+    oidc.logout()
+    return redirect(url_for('index.index'))
 
 
-@recommend_bp.route("/recommendDetail", methods=["GET"])
-def recommendDetail():
-    eid = request.args.get("eid", default=None, type=int)
-    tid = request.args.get("tid", default=None, type=int)
-    team = request.args.get("team", default=1, type=int)
-    basic_info = detail_service.recommendDetail(eid=eid, tid=tid, team=team)
-    return render_template("social_network/compareTeacherAndEngineerTeam.html", eid=eid, tid=tid, team=team, basic_info=basic_info)
+@index_bp.route("/org-info")
+def getOrgInfo():
+    name = request.args.get("name", default="")
+    org_type = request.args.get("type", default="")
+    return public_service.getOrgId(label=org_type, name=name)
 
 
-@recommend_bp.route("/technicalFieldComparison")
-def technicalFieldComparison():
-    eid = request.args.get("eid", default=None, type=int)
-    tid = request.args.get("tid", default=None, type=int)
-    team = request.args.get("team", default=1, type=int)
-    return detail_service.technicalFieldComparison(eid=eid, tid=tid, team=team)
+@index_bp.route("/personal-network")
+@oidc.require_login
+def personalNetwork():
+    return render_template("social_network/personal_network.html")
 
 
-@recommend_bp.route("/teamMembers")
-def getTeamMembers():
-    eid = request.args.get("eid", default=None, type=int)
-    tid = request.args.get("tid", default=None, type=int)
-    return detail_service.getTeamMembers(team_t=tid, team_e=eid)
+@index_bp.route("/getPersonalNetwork")
+@oidc.require_login
+def getPersonalNetwork():
+    # TODO 动态获取中介 id 及 类型
+    agent_id = auth.getUserId()
+    agent_type = "uni" if auth.require_role("KETD", "技转中心") else "area"
+    return personal_service.getPersonalNetwork(agent_id=agent_id, agent_type=agent_type)
