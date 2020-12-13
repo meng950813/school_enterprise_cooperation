858651becfd57704f2e12a31db9acf4f5ba30530
i2lmeng <1752397568@qq.com>
2020-12-13 16:46:34 +0800

    modify: 拆分修改用户与 专家/工程师关系的操作到api中

--- web/dao/social_network/link_path.py
+++ web/dao/social_network/link_path.py
@@ -1,5 +1,6 @@
 """
-获取两个节点之间的联络路径
+获取两个节点之间的联络路径, 具体情况包括：
+
 """
 from web.settings import RELATION
 from web.utils.neo4j_operator import neo4j as neo4j
@@ -68,19 +69,3 @@ def getLinkPath(agent_id, agent_label, target_id, target_label, step=3, limit=5)
         .format(agent_label=agent_label, agent_id=agent_id, target_id=target_id, target_label=target_label, step=step,
                 limit=limit)
     return neo4j.run(cql)
-
-
-def addContactInformation(start_node, target_node, visited=0, cooperate=0, activity=0):
-    """
-    添加联络信息： 使用 py2neo & neo4j_operator 封装的函数
-    若 该关系尚不存在 => 创建关系， 否则 => 在原有基础上改变属性值
-    :return: True or False
-    """
-    relationship = RELATION["KNOWS"]
-    relation = neo4j.search_relationship(start_node=start_node, target_node=target_node, relationship=relationship)
-    if relation is None:
-        return neo4j.create_relationship(start_node=start_node, target_node=target_node, relationship=relationship,
-                                         visited=visited, cooperate=cooperate, activity=activity)
-    else:
-        return neo4j.update_one_relationship(start_node=start_node, target_node=target_node, relation=relation,
-                                             cover=False, visited=visited, cooperate=cooperate, activity=activity)
