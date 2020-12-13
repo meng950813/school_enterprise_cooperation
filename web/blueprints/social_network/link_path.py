463f0571ba83fe20bff3fbc643ac58fc63027120
i2lmeng <1752397568@qq.com>
2020-12-13 15:27:50 +0800

    modify: 从keyclock中获取用户信息

--- web/blueprints/social_network/link_path.py
+++ web/blueprints/social_network/link_path.py
@@ -1,15 +1,21 @@
 from flask import Blueprint, request
 from web.service.social_network import link_path as path_service
+from web.utils import auth
 
 link_path_bp = Blueprint('link_path', __name__)
 
 
 @link_path_bp.route("/path")
 def getLinkPath():
-    # TODO 待获取数据，
-    agent_id = 1  # 从登陆信息中获取
-    agent_type = "uni"  # 从登陆信息中获取
-    target_id = request.args.get("target", default=0, type=int)
+    agent_id = auth.getUserId()
+    # agent_id = "jizhuan1"
+    agent_type = ""  # 从登陆信息中获取
+    if auth.isUniversityAgent():
+        agent_type = "uni"
+    elif auth.isAreaAgent():
+        agent_type = "area"
+
+    target_id = request.args.get("target", default="", type=str)
     target_type = request.args.get("t_type", default="teacher", type=str)
     step = request.args.get("step", default=3, type=int)
     limit = request.args.get("limit", default=5, type=int)
